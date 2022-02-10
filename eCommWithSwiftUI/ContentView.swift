4b425fd6f3187157510729f3a36686c11cbc03f6
Chetan Patil <cvpatil1912@gmail.com>
2022-02-10 13:44:51 +0530

    Initial Commit

---
+++ eCommWithSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  eCommWithSwiftUI
+//
+//  Created by Chetan Patil on 10/02/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ eCommWithSwiftUI/eCommWithSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  eCommWithSwiftUIApp.swift
+//  eCommWithSwiftUI
+//
+//  Created by Chetan Patil on 10/02/22.
+//
+
+import SwiftUI
+
+@main
+struct eCommWithSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
